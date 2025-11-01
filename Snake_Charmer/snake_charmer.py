59fb332860c8647bf64a96c672b0bc0cf00ccbf3
Mr.Lockpick <gat2835@rit.edu>
2025-10-31 20:11:37 -0400

    added os switch to include windows

--- Snake_Charmer/snake_charmer.py
+++ Snake_Charmer/snake_charmer.py
@@ -60,52 +60,38 @@ def make_target_list(ip_string, variable_list): # pass it a string to be modifie
         to_return.append(str(ip_string).replace('x',str(number)))
     return to_return
 
+#RUNS THE MINIONS, also does the OS switch
+def run_multiple_multithread(ip_list,username,password,command,os):
 
-
-#RUNS THE MINIONS
-def run_multiple_multithread(ip_list,username,password,command):
-    with ThreadPoolExecutor(max_workers=WORKERS) as executor:
-        for ip in ip_list:
-            executor.submit(single_connection_command_linux, ip,username,password,command)
-
+    if os == ("L" or "Linux"):
+        with ThreadPoolExecutor(max_workers=WORKERS) as executor:
+            for ip in ip_list:
+                executor.submit(single_connection_command_linux, ip,username,password,command)
+    elif os == ("W" or "Windows"):
+        with ThreadPoolExecutor(max_workers=WORKERS) as executor:
+            for ip in ip_list:
+                executor.submit(single_connection_command_windows,ip,username,password,command)
 
 #ATTACK FUNCTIONS
-
-def team_attack(teamnumber,username,password,command):
+def team_attack(teamnumber,username,password,command,os):
     targets=make_target_list("192.168."+str(teamnumber)+".x",[1,2])
     targets2=make_target_list("10."+str(teamnumber)+".1."+'x',[4,5,6])
     targets=targets+targets2
-    run_multiple_multithread(targets,username,password,command)
+    run_multiple_multithread(targets,username,password,command,os)
 
-def box_attack(box_hostname,username,password,command):
+def box_attack(box_hostname,username,password,command,os):
     targets=list()
     if HOSTNAME_DICT[box_hostname] <3:
         targets=make_target_list("192.168.x."+str(HOSTNAME_DICT[box_hostname]),list(range(1,19)))
     else:
         targets=make_target_list("10.x.1."+str(HOSTNAME_DICT[box_hostname]),list(range(1,19)))
-    run_multiple_multithread(targets,username,password,command)
+    run_multiple_multithread(targets,username,password,command,os)
         
-def all_attack(username,password,command):
+def all_attack(username,password,command,os):
     for key in HOSTNAME_DICT:
-        box_attack(key,username,password,command)
+        box_attack(key,username,password,command,os)
 
-#MAKES THE CUSTOM IPS TWO WILDCARD
-def make_target_list_two_var(ip_string, variable_list_x, variable_list_y): 
-    to_return=list()
-    for number_x in variable_list_x:
-        for number_y in variable_list_y:
-            to_return.append(str(ip_string).replace('x',str(number_x)).replace('y',str(number_y)))
-    return to_return
-
-def wildcard(input,ip_dict,username,password,command):
-    # pass#TODO implement
-    targets=list()
-    for ip_wildcard in ip_dict:
-        print(input)
-        input=input[:ip_wildcard-1]+input[ip_wildcard+2:]
-        print(input)
-
-def expand_wildcards(ip_template, wildcard_dict,username,password,command):
+def expand_wildcards(ip_template,username,password,command,os): #pos 2: wildcard_dict,
 
     parts = ip_template.split('.')
     ranges = []
@@ -129,58 +115,53 @@ def expand_wildcards(ip_template, wildcard_dict,username,password,command):
 
     backtrack()
     print(expanded_ips)
-    run_multiple_multithread(expanded_ips,username,password,command)
+    run_multiple_multithread(expanded_ips,username,password,command,os)
+    
+    
 
+
+  
 #CLI
 def cli_interface():
     print("\n ~~ SNAKE CHARMER ~~\n")
     
-    #Basic input fields
-    os=input("Enter OS (Linux or Windows): ").upper()
-    if os == "LINUX" or os == "L":
-        username=input("Target user: ")
-        password=input("Target Password (leave blank for default): ")
-        if password=="":
-            password="Change.me123!"
-        print("Using credentials ",username,":",password,sep="")
-        command=input("\nCommand to run: ")
-        targets_input = input("Enter either a: \n-Single IP address (192.168.1.1)\n-A single team name ('team01','team2', ... ,'team18'), \n-Single box type ('Big Bang, Dino Asteroid, Viking Raids, Enlightenment, Chernobyl')\n-All ('All')\nInput: ").upper()
-        box_targets=['BIG BANG','DINO ASTEROID','VIKING RAIDS','ENLIGHTENMENT','CHERNOBYL']
-        if "TEAM" in targets_input:
-            if targets_input[-2:-1]=="1":
-                teamnum=targets_input[-2:]
-            else:
-                teamnum=targets_input[-1:]
-            if (int(teamnum)>19 and int(teamnum)<0):
-                raise IndexError("Team number is not between 1 and 18")
-            team_attack(teamnum,username,password,command)
+    print("Invalid input! Quitting")
+    username=input("Target user: ")
+    password=input("Target Password (leave blank for default): ")
+    if password=="":
+        password="Change.me123!"
+    print("Using credentials ",username,":",password,sep="")
+    command=input("\nCommand to run: ")
+    os=input("Linux or Windows").upper()
 
-        elif any(box in targets_input for box in box_targets):
-            box_attack(targets_input,username,password,command)
-        elif "ALL" in targets_input:
-            all_attack(username,password,command)
-        elif "-" in targets_input:
-            tack_index=[i for i, c in enumerate(targets_input) if c == "-"]
-            tack_dict=dict()
-            for tack in tack_index: 
-                tack_dict[tack]=(list(range(int(targets_input[tack-1:tack]),int(targets_input[tack+1:tack+2])+1)))
+    targets_input = input("Enter either a: \n-Single IP address (192.168.1.1)\n-A single team name ('team01','team2', ... ,'team18'), \n-Single box type ('Big Bang, Dino Asteroid, Viking Raids, Enlightenment, Chernobyl, etc')\n-All ('All')\nInput: ").upper()
+    box_targets=['BIG BANG','DINO ASTEROID','VIKING RAIDS','ENLIGHTENMENT','CHERNOBYL']
+    box_targets_windows=['WRIGHT BROTHERS','MOON LANDING','PYRAMIDS','FIRST OLYMPICS','SILK ROAD']
 
-            # wildcard(targets_input,tack_dict,username,password,command)
-            expand_wildcards(targets_input,tack_dict,username,password,command)
-
-        elif (re.match(r"^(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$",targets_input)) or targets_input=="LOCALHOST":
-            single_connection_command_linux(targets_input,username,password,command) 
-        
+    if "TEAM" in targets_input:
+        if targets_input[-2:-1]=="1":
+            teamnum=targets_input[-2:]
         else:
-            print("No matching entries!!")
+            teamnum=targets_input[-1:]
+        if (int(teamnum)>19 and int(teamnum)<0):
+            raise IndexError("Team number is not between 1 and 18")
+        
+        team_attack(teamnum,username,password,command,os)
 
-    elif input == "WINDOWS" or input == "W":
-        pass
+    elif any(box in targets_input for box in box_targets):
+        box_attack(targets_input,username,password,command,os)
+    elif "ALL" in targets_input:
+        all_attack(username,password,command,os)
+    elif "-" in targets_input:
+        
+        # wildcard(targets_input,tack_dict,username,password,command)
+        expand_wildcards(targets_input,username,password,command,os)
 
+    elif (re.match(r"^(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$",targets_input)) or targets_input=="LOCALHOST":
+        single_connection_command_linux(targets_input,username,password,command,os) 
 
     else:
-        print("Invalid input! Quitting")
-    
+        print("No matching entries!!")
 
         
 def main():
@@ -192,6 +173,72 @@ if __name__=="__main__":
 
 '''
 code graveyard
+
+
+tack_index=[i for i, c in enumerate(targets_input) if c == "-"]
+        tack_dict=dict()
+        for tack in tack_index: 
+            tack_dict[tack]=(list(range(int(targets_input[tack-1:tack]),int(targets_input[tack+1:tack+2])+1)))
+
+
+
+def wildcard(input,ip_dict,username,password,command,):
+    # pass#TODO implement
+    targets=list()
+    for ip_wildcard in ip_dict:
+        print(input)
+        input=input[:ip_wildcard-1]+input[ip_wildcard+2:]
+        print(input)
+
+
+    elif input == "WINDOWS" or input == "W":
+        username=input("Target user: ")
+        password=input("Target Password (leave blank for default): ")
+        if password=="":
+            password="Change.me123!"
+        print("Using credentials ",username,":",password,sep="")
+        command=input("\nCommand to run: ")
+        targets_input = input("Enter either a: \n-Single IP address (192.168.1.1)\n-A IP address range (192.168.1-10.5, 10.1.3-9.4-33)\n-A single team name ('team01','team2', ... ,'team18'), \n-Single box type ('Big Bang, Dino Asteroid, Viking Raids, Enlightenment, Chernobyl')\n-All ('All')\nInput: ").upper()
+        box_targets=['WRIGHT BROTHERS','MOON LANDING','PYRAMIDS','FIRST OLYMPICS','SILK ROAD']
+        if "TEAM" in targets_input:
+            if targets_input[-2:-1]=="1":
+                teamnum=targets_input[-2:]
+            else:
+                teamnum=targets_input[-1:]
+            if (int(teamnum)>19 and int(teamnum)<0):
+                raise IndexError("Team number is not between 1 and 18")
+            team_attack_windows(teamnum,username,password,command)
+
+        elif any(box in targets_input for box in box_targets):
+            box_attack_windows(targets_input,username,password,command)
+        elif "ALL" in targets_input:
+            all_attack_windows(username,password,command)
+        elif "-" in targets_input:
+            tack_index=[i for i, c in enumerate(targets_input) if c == "-"]
+            tack_dict=dict()
+            for tack in tack_index: 
+                tack_dict[tack]=(list(range(int(targets_input[tack-1:tack]),int(targets_input[tack+1:tack+2])+1)))
+
+            # wildcard(targets_input,tack_dict,username,password,command)
+            expand_wildcards_windows(targets_input,tack_dict,username,password,command)
+
+        elif (re.match(r"^(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$",targets_input)) or targets_input=="LOCALHOST":
+            single_connection_command_linux(targets_input,username,password,command) 
+        
+        else:
+            print("No matching entries!!")
+
+
+
+
+
+#MAKES THE CUSTOM IPS TWO WILDCARD
+def make_target_list_two_var(ip_string, variable_list_x, variable_list_y): 
+    to_return=list()
+    for number_x in variable_list_x:
+        for number_y in variable_list_y:
+            to_return.append(str(ip_string).replace('x',str(number_x)).replace('y',str(number_y)))
+    return to_return
 
 
 
